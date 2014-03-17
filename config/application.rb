48df33d3987c6633cf19a44cbbcf099b483c4d73
Caladan Cadarn <stevao.cal@gmail.com>
2014-03-17 10:21:24 -0300

    primeira tentativa do dia

--- config/application.rb
+++ config/application.rb
@@ -17,8 +17,6 @@ module Litterae02
     # config.time_zone = 'Central Time (US & Canada)'
     config.time_zone = 'Brasilia'
 
-    config.i18n.enforce_available_locales = true
-
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
