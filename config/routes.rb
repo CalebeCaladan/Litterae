ba8f86dc57742076fd1ffbe4afff3676b3a2d69a
Caladan Cadarn <stevao.cal@gmail.com>
2014-03-27 14:45:28 -0300

    arrumando rota

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,14 @@ Litterae02::Application.routes.draw do
   get "static_page/bemvindo", path: 'boasvindas'
   get "static_page/help", path: 'ajuda'
   mount Bootsy::Engine => '/bootsy', as: 'bootsy'
-  resources :users
-  resources :categories
-  resources :wordings do
+  resources :users, path: 'usuarios'
+  resources :categories, path: 'categorias'
+  resources :wordings, path: 'resenhas' do
     member { post :vote}
   end
 
 
-  root to: 'wordings#index'
+  root to: 'static_page#bemvindo'
 
   match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   match 'auth/failure', to: redirect('/'), via: [:get, :post]
